722e2d8e300c0583ced281d1afd437b74b52f55b
Alan <castillo052899@gmail.com>
2021-10-09 19:34:19 -0500

    cambio html

--- js/nuevo.js
+++ js/nuevo.js
@@ -1,26 +1,6 @@
 const btn = document.querySelector('.btnCortina');
 
 
-const typed = new Typed('.typed', {
-    strings: [
-        '<i class="palabra_spanA">Alfredo Castillo</i>', 
-        '<i class="palabra_spanV">Desarrollador</i>',
-        '<i class="palabra_spanV">Front-End</i>'
-    ],
-    // stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
-	startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
-	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
-	// smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
-	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 1000, // Tiempo de espera despues de que termina de escribir una palabra.
-	loop: true, // Repetir el array de strings
-	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
-	showCursor: true, // Mostrar cursor palpitanto
-	cursorChar: '|', // Caracter para el cursor
-	contentType: 'html', // 'html' o 'null' para texto sin formato
-}); 
-
 /* -----------------------------------------------------------------------Efectos cuando la pantalla se carga */
 function logoLinea(){
 	let tl = gsap.timeline({
@@ -49,16 +29,6 @@ function logoLinea(){
 		ease: 'back'
 
 	} ,'-=1' );
-
-	tl.to('.imgL', {
-	    duration: 1,
-		repeat: -1,
-	    y: -5, 
-	    stagger: 0.5,
-		// ease: 'back',
-		delay: 0.5,
-		yoyo: true
-	});
 }
 
 logoLinea();
